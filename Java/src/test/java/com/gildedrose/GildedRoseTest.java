0a760c1541587167fffe2568900865482ec73da2
marco <mrc.tst@gmail.com>
2024-01-25 18:23:30 +0100

    test: test aged Brie

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,27 @@
 package com.gildedrose;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
-    @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    @ParameterizedTest
+    @CsvSource({"1,-1,0", "2,-1,0", "5,-1,3"})
+    void foo(int quality, int expectedSellIn, int expectedQuality) {
+        Item[] items = new Item[] { new Item("foo", 0, quality) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo, "+expectedSellIn+", "+expectedQuality, app.items[0].toString());
+    }
+    @Test
+    void agedBrie() {
+        Item[] items = new Item[] { new Item("Aged Brie", 0, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Aged Brie, -1, 2", app.items[0].toString());
     }
 
 }
